56ab7a0286cc3c7850418097b72e6b31556437cd
Maarthewolf <miksanek.m@email.cz>
2024-03-11 17:11:54 +0100

    dunno

---
+++ src/main/java/net/maarthewolf/tutorialmod/item/ModCreativeModTabs.java
@@ -0,0 +1,30 @@
+package net.maarthewolf.tutorialmod.item;
+
+import net.maarthewolf.tutorialmod.TutorialMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import org.apache.http.message.BasicHeaderValueParser;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("totorial_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+                    .title(Component.translatable("creativetab.tutorial_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+
+                        output.accept(ModItems.SAPPHIRE.get());
+
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
